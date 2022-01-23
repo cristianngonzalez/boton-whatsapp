e91b8e34634dcff0af275fc3fbce8a43f3ab2e3f
cristianngonzalez <95558857+cristianngonzalez@users.noreply.github.com>
2022-01-22 21:02:41 -0300

    Added copy buttons

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ var app = new Vue({
       countrySelected: '+54',
       phoneNumber: '',
       color: '#00CC33',
+    },
+    methods:{
+      /*To copy the code*/
+      copyToClipBoard(event , id){
+        //Restore all copy buttons
+        let buttons = document.getElementsByClassName('copyButton')
+        buttons[0].innerHTML = 'Copy';
+        buttons[1].innerHTML = 'Copy';
+
+        //Change text this copy button
+        event.target.innerHTML = 'Copied!'
+
+        //Copy to clipboard
+        const str = document.getElementById(id);
+        window.getSelection().selectAllChildren(str);
+        document.execCommand("Copy")
+      }
     }
 
 
